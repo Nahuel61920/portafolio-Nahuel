9fd1fadafec3287fd2a62b0c0b2cce311ac52c3e
Esteban Nahuel Carrizo <estebancarrizo619@gmail.com>
2022-04-26 01:56:21 -0300

    Adding particles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './App.css';
+import ParticlesBackground from './components/Particles/ParticlesBackground'
 
 /* Componet */
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main';
 import Contenido from './components/Contenido';
-
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 
 function App() {
     return (
         <div className='App'>
             <Header/>
-            <div id="particles-js"></div>
 
             <Contenido/>
+
+            <ParticlesBackground />
 
             <Main/>
 
---
+++ src/components/Particles/ParticlesBackground.js
@@ -0,0 +1,10 @@
+import Particles from "react-tsparticles";
+import particlesConfig from "./particle-config";
+
+const ParticlesBackground = () => {
+    return (
+        <Particles params={particlesConfig}></Particles>
+    )
+};
+
+export default ParticlesBackground
